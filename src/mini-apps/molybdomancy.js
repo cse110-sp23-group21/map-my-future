b7d91b99e8be055f1324e13b889b2bb40d878c06
Gil Keidar <keidar.gil@gmail.com>
2023-05-21 18:06:08 -0700

    Added test dictionaries for 2 mini-apps
    Added stand-in dictionaries for Cartomancy and Molybdomancy
    
    Co-Authored-By: MinhDuong <64678096+minhnhat1901@users.noreply.github.com>

--- src/mini-apps/molybdomancy.js
+++ src/mini-apps/molybdomancy.js
@@ -1,2 +1,24 @@
 /*  Molybdomancy Code Here  */
 
+//  List of shapes
+const shapes = [
+    {
+        name: "Algae",
+        description: "A circle with most of the metal on the ends rather than the center",
+        img: "algae.jpg",
+        meaning: "Longing for the Sea",
+    },
+    {
+        name: "Algae2",
+        description: "A circle with most of the metal on the ends rather than the center",
+        img: "algae.jpg",
+        meaning: "Longing for the Sea",
+    },
+    {
+        name: "Algae3",
+        description: "A circle with most of the metal on the ends rather than the center",
+        img: "algae.jpg",
+        meaning: "Longing for the Sea",
+    }
+];
+
