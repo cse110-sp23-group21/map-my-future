f2bf0e0064007622524d4a2d21b9bad90b9d8ebd
Gil Keidar <keidar.gil@gmail.com>
2023-05-21 20:33:17 -0700

    test

--- src/engine.js
+++ src/engine.js
@@ -1,1 +1,14 @@
 /*  Place 8Ball Engine Code Here */
+
+/**
+ * Demo function - adds two input numbers and return result
+ * 
+ * @param n
+ * @param m
+ * @returns n + m
+ * @author Gil Keidar
+ */
+function demo(n, m)
+{
+    return Number(n) + Number(m);
+}
