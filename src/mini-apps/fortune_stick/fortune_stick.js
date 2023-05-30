c3862cb56e2a7a09e429e63339ec74bd4b13c6de
justin-chiang <59491073+justin-chiang@users.noreply.github.com>
2023-05-29 17:22:18 -0700

    template ui for mini apps

--- src/mini-apps/fortune_stick/fortune_stick.js
+++ src/mini-apps/fortune_stick/fortune_stick.js
@@ -1,1 +1,34 @@
-/* Fortune Stick code goes here */
+// all driver code should be within this event listener, ie adding other event listeners and calling on imported engine
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Background music
+  const bgm = new Audio('../../../assets/map-my-future-bgm.ogg'); //  eslint-disable-line
+  bgm.play();
+  bgm.loop = true;
+
+  // Buttons
+  let musicEnabled = true;
+  let showInfo = false;
+  const musicButton = document.getElementById('music-button');
+  const infoButton = document.getElementById('info-button');
+
+  musicButton.addEventListener('click', (e) => {
+    console.log('test');
+    const musicImg = document.querySelectorAll('img')[0];
+    if (musicEnabled) {
+      musicImg.src = '../../../assets/audio_off.png';
+      bgm.pause();
+    } else {
+      musicImg.src = '../../../assets/audio_on.png';
+      bgm.play();
+    }
+    musicEnabled = !musicEnabled;
+  });
+
+  infoButton.addEventListener('click', (e) => {
+    const infoPopup = document.getElementById('info-popup');
+    infoPopup.style.display = !showInfo ? 'flex' : 'none';
+    showInfo = !showInfo;
+  });
+})
