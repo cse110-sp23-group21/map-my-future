896dd458a429b34378b986913bfdb2e2b1411f0f
Jun Linwu <julinwu@ucsd.edu>
2023-06-08 03:58:39 -0700

    Minor change on fortune stick instruction.

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
           sidePanel.style.backgroundImage = 'url(../assets/side-moly.png)';
           panelLayout.style.marginTop = '40%';
         } else if (locationName === 'Fortune Stick') {
-          instructionTxt.innerHTML = 'Fortune sticks, also known as Chinese fortune sticks or divination sticks, are a traditional method of seeking guidance and insight from Chinese culture. </br></br> In this method of fortune-telling, you will click to shake the container and retrieve a single fortune stick.';
+          instructionTxt.innerHTML = 'Fortune sticks, also known as Chinese fortune sticks or divination sticks, are a traditional method of seeking guidance and insight from Chinese culture. </br></br> In this method of fortune-telling, you will click to shake the container and retrieve a single fortune stick. </br></br> An interpretation will be generated at the end of each round.';
           sidePanel.style.backgroundImage = 'url(../assets/side-stick.png)';
           panelLayout.style.marginTop = '60%';
         } else if (locationName === 'Cartomancy') {
