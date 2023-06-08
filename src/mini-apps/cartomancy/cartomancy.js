f5197716947ef0e36ebece6c5a0216dd5999be15
maarongross <97714828+maarongross@users.noreply.github.com>
2023-06-07 21:13:30 -0700

    Update cartomancy.js

--- src/mini-apps/cartomancy/cartomancy.js
+++ src/mini-apps/cartomancy/cartomancy.js
@@ -1,14 +1,13 @@
 // all driver code should be within this event listener, ie adding other event listeners and calling on imported engine
 
-import FortuneEngine from "../../engine.js";
+import FortuneEngine from '../../engine.js';
 
 const engine = new FortuneEngine();
-const APP_NAME = "cartomancy";
+const APP_NAME = 'cartomancy';
 
 let cardsPicked = 0; // counter when 3 cards are picked
 
 document.addEventListener('DOMContentLoaded', async () => {
-
   // Read JSON File
   // Taken of: https://www.thetarotguide.com/
   await engine.db_reader(`./${APP_NAME}.json`);
@@ -25,44 +24,40 @@ document.addEventListener('DOMContentLoaded', async () => {
   // buttons
   const musicButton = document.getElementById('music-button');
   const infoButton = document.getElementById('info-button');
-  const readFortuneButton = document.getElementById("read-fortune-button");
-  const startButton = document.getElementById('start-button'); 
+  const readFortuneButton = document.getElementById('read-fortune-button');
+  const startButton = document.getElementById('start-button');
 
   const origDeck = document.getElementById('card-display');
   const center_div = document.getElementById('center-div');
 
-
-  startButton.addEventListener('click', (e) => {    
-    console.log("start");
-    document.getElementById('intro').style.display = "none"; 
-    origDeck.style.display = "initial";
-    document.getElementById('start-button').style.display = "none";
-    document.getElementById('pickContainer1').style.display = "flex";
-    document.getElementById('pickContainer2').style.display = "flex";
-    document.getElementById('pickContainer3').style.display = "flex";
+  startButton.addEventListener('click', (e) => {
+    console.log('start');
+    document.getElementById('intro').style.display = 'none';
+    origDeck.style.display = 'initial';
+    document.getElementById('start-button').style.display = 'none';
+    document.getElementById('pickContainer1').style.display = 'flex';
+    document.getElementById('pickContainer2').style.display = 'flex';
+    document.getElementById('pickContainer3').style.display = 'flex';
   });
 
   // button display:'flex' after 3 cards
-  readFortuneButton.addEventListener("click", (e) => {
-
-    //console.log("Yeah you loaded thsi many cards:");
+  readFortuneButton.addEventListener('click', (e) => {
+    // console.log("Yeah you loaded thsi many cards:");
 
     // put away cards
     console.log(origDeck);
-    origDeck.classList.add("hide-cards");
+    origDeck.classList.add('hide-cards');
 
-    console.log("should of begun hiding cards");
+    console.log('should of begun hiding cards');
 
     setTimeout(() => {
-
       origDeck.remove();
-      console.log("turned display attr off");
+      console.log('turned display attr off');
 
       // start reading cards
-      readCards(center_div); 
-
+      readCards(center_div);
     }, 1000);
-  }); 
+  });
 
   musicButton.addEventListener('click', (e) => {
     console.log('music');
@@ -70,8 +65,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (musicEnabled) {
       musicImg.src = '/assets/audio_off.png';
       bgm.pause();
-    }
-    else {
+    } else {
       musicImg.src = '/assets/audio_on.png';
       bgm.play();
     }
@@ -91,15 +85,15 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 });
 
-// Card selector counter 
-let i = 0; 
-let pickCounter1 = 0; 
-let pickCounter2 = 0; 
-let pickCounter3 = 0; 
+// Card selector counter
+let i = 0;
+const pickCounter1 = 0;
+const pickCounter2 = 0;
+const pickCounter3 = 0;
 
-// Allows cards to be selectable and dragged to satisfied palce 
-let sourceContainerID =''; 
-let droppedID =''; 
+// Allows cards to be selectable and dragged to satisfied palce
+let sourceContainerID = '';
+let droppedID = '';
 const card1s = document.querySelectorAll('.card1');
 const card2s = document.querySelectorAll('.card2');
 const card3s = document.querySelectorAll('.card3');
@@ -108,97 +102,94 @@ const pickContainer2 = document.getElementById('pickContainer2');
 const pickContainer3 = document.getElementById('pickContainer3');
 
 const card1sArray = Array.from(card1s);
-const card2sArray = Array.from(card2s); 
+const card2sArray = Array.from(card2s);
 const card3sArray = Array.from(card3s);
 
 card1s.forEach(card => {
-  card.addEventListener('dragstart', dragStart); 
-})
+  card.addEventListener('dragstart', dragStart);
+});
 card2s.forEach(card => {
-  card.addEventListener('dragstart', dragStart); 
-}); 
+  card.addEventListener('dragstart', dragStart);
+});
 card3s.forEach(card => {
-  card.addEventListener('dragstart', dragStart); 
-})
+  card.addEventListener('dragstart', dragStart);
+});
 
-pickContainer1.addEventListener('drop', dropped); 
-pickContainer1.addEventListener('dragenter', cancelDefault); 
-pickContainer1.addEventListener('dragover', cancelDefault); 
+pickContainer1.addEventListener('drop', dropped);
+pickContainer1.addEventListener('dragenter', cancelDefault);
+pickContainer1.addEventListener('dragover', cancelDefault);
 
-pickContainer2.addEventListener('drop', dropped); 
-pickContainer2.addEventListener('dragenter', cancelDefault); 
-pickContainer2.addEventListener('dragover', cancelDefault); 
+pickContainer2.addEventListener('drop', dropped);
+pickContainer2.addEventListener('dragenter', cancelDefault);
+pickContainer2.addEventListener('dragover', cancelDefault);
 
-pickContainer3.addEventListener('drop', dropped); 
-pickContainer3.addEventListener('dragenter', cancelDefault); 
-pickContainer3.addEventListener('dragover', cancelDefault); 
+pickContainer3.addEventListener('drop', dropped);
+pickContainer3.addEventListener('dragenter', cancelDefault);
+pickContainer3.addEventListener('dragover', cancelDefault);
 
 // TODO: Documentation for function
 function cancelDefault (e) {
-  e.preventDefault(); 
-  e.stopPropagation(); 
-  return false; 
+  e.preventDefault();
+  e.stopPropagation();
+  return false;
 }
 
 // TODO: Documentation for function
 function dragStart (e) {
-  e.dataTransfer.setData('text/plain', e.target.id); 
-  sourceContainerID = this.parentElement.id; 
-  droppedID = this.id; 
+  e.dataTransfer.setData('text/plain', e.target.id);
+  sourceContainerID = this.parentElement.id;
+  droppedID = this.id;
 }
 
 // TODO: Documentation for function
 function dropped (e) {
   if (this.id !== sourceContainerID) {
-    cancelDefault(e); 
-    let id = e.dataTransfer.getData('text/plain'); 
+    cancelDefault(e);
+    const id = e.dataTransfer.getData('text/plain');
     if (droppedID == 'card1') {
-      if((e.target.id == 'pickcontainer1' || e.target.id == 'pickcontainer2' || e.target.id == 'pickcontainer3') || (e.target.id !== 'card1' && e.target.id !== 'card2' && e.target.id !== 'card3')){
+      if ((e.target.id == 'pickcontainer1' || e.target.id == 'pickcontainer2' || e.target.id == 'pickcontainer3') || (e.target.id !== 'card1' && e.target.id !== 'card2' && e.target.id !== 'card3')) {
         const droppedElement = card1sArray[i];
         e.target.appendChild(card1sArray[i]);
-        droppedElement.draggable = false; 
+        droppedElement.draggable = false;
         cardsPicked++;
-        i++;  
+        i++;
       }
-    } 
-    else if (droppedID == 'card2') {
+    } else if (droppedID == 'card2') {
       if ((e.target.id == 'pickcontainer1' || e.target.id == 'pickcontainer2' || e.target.id == 'pickcontainer3') || (e.target.id !== 'card1' && e.target.id !== 'card2' && e.target.id !== 'card3')) {
-      const droppedElement = card2sArray[i];
-      e.target.appendChild(card2sArray[i]);
-      droppedElement.draggable = false; 
-      cardsPicked++;
-      i++;
+        const droppedElement = card2sArray[i];
+        e.target.appendChild(card2sArray[i]);
+        droppedElement.draggable = false;
+        cardsPicked++;
+        i++;
       }
-    } 
-    else if(droppedID == 'card3') {
-      if((e.target.id == 'pickcontainer1' || e.target.id == 'pickcontainer2' || e.target.id == 'pickcontainer3') || (e.target.id !== 'card1' && e.target.id !== 'card2' && e.target.id !== 'card3')) {
-      const droppedElement = card3sArray[i];
-      e.target.appendChild(card3sArray[i]);
-      droppedElement.draggable = false; 
-      cardsPicked++; 
-      i++; 
+    } else if (droppedID == 'card3') {
+      if ((e.target.id == 'pickcontainer1' || e.target.id == 'pickcontainer2' || e.target.id == 'pickcontainer3') || (e.target.id !== 'card1' && e.target.id !== 'card2' && e.target.id !== 'card3')) {
+        const droppedElement = card3sArray[i];
+        e.target.appendChild(card3sArray[i]);
+        droppedElement.draggable = false;
+        cardsPicked++;
+        i++;
       }
+    }
   }
-}
 
   // Enable reset button after card is picked
-  if(cardsPicked == 1) {
+  if (cardsPicked == 1) {
     document.querySelector('#reset-button').style.display = 'flex';
   }
 
   // Once user selects 3 cards, their fortune can be read
-  if(cardsPicked == 3) {
+  if (cardsPicked == 3) {
     // assumes document is loaded
-    //console.log("reached 3 cards loaded");
+    // console.log("reached 3 cards loaded");
     document.querySelectorAll('.read-fortune-space')[0].style.display = 'flex';
   }
-
 }
 
 /*
  * Begin the fortune telling process by reading the chosen cards
  */
-function readCards(center_div) {
+function readCards (center_div) {
   // animations, grouping etc here
   center_div.classList.add('container');
 
@@ -219,25 +210,22 @@ function readCards(center_div) {
   const pickContainer2 = document.getElementById('pickContainer2');
   const pickContainer3 = document.getElementById('pickContainer3');
 
-
   // actually may better loop later
   let i = 1;
-  while(i < 4) { // loop through first 3
-    let picking = document.getElementById(`pickContainer${i}`);
-    console.log("trying to send another container");
+  while (i < 4) { // loop through first 3
+    const picking = document.getElementById(`pickContainer${i}`);
+    console.log('trying to send another container');
     console.log(picking);
-    organizeCards(picking, receivedFortunes[i-1]);
+    organizeCards(picking, receivedFortunes[i - 1]);
     i++;
   }
-
 }
-
 
 /*
  * Creates the elements surrounding the dsiplaying of the three fortunes
  */
-function organizeCards(pick, fortune) {
-  /***** creates the following html elements with js
+function organizeCards (pick, fortune) {
+  /** *** creates the following html elements with js
    *
    *  <div class = card>
    *    <div class = image>
@@ -247,36 +235,35 @@ function organizeCards(pick, fortune) {
    *      <h3>name</h3>
    *      <p>information</p>
    *    </div>
-   *  </div> 
+   *  </div>
    *
    *
    */
-  /***** After writing, should look into finding a way to load better  ****/ 
+  /** *** After writing, should look into finding a way to load better  ****/
   console.log(pick.firstChild);
   pick.removeChild(pick.firstChild);
 
   pick.classList.remove('pickContainer');
   pick.classList.add('read-container');
 
-  console.log("should of begun hiding cards");
-  //console.log(pick);
-  //console.log(fortune);
+  console.log('should of begun hiding cards');
+  // console.log(pick);
+  // console.log(fortune);
 
-  let indCard = document.createElement('div');
-  let indCardImageContainer = document.createElement('div');
-  let indCardImage = document.createElement('img');
-  let content = document.createElement('div');
-  let fDescr = document.createElement('p');
-  let node = document.createTextNode(fortune['result']); // add fortune text
+  const indCard = document.createElement('div');
+  const indCardImageContainer = document.createElement('div');
+  const indCardImage = document.createElement('img');
+  const content = document.createElement('div');
+  const fDescr = document.createElement('p');
+  const node = document.createTextNode(fortune.result); // add fortune text
 
   // add attributes
   fDescr.classList.add('read-fortune');
   indCard.classList.add('cardShow');
   indCardImageContainer.classList.add('image');
-  indCardImage.setAttribute("href", "#"); 
-  indCardImage.setAttribute("src", `/assets/${APP_NAME}/${fortune['image']}.png`); 
+  indCardImage.setAttribute('href', '#');
+  indCardImage.setAttribute('src', `/assets/${APP_NAME}/${fortune.image}.png`);
   content.classList.add('content');
-
 
   // nest them by adding as children
   fDescr.appendChild(node);
