f959f6714726d7bf5f22bbb99548ec67f8f68c18
Jun Linwu <32351528+juxx8@users.noreply.github.com>
2023-06-08 00:34:15 -0700

    remove drag functionality

--- src/app.js
+++ src/app.js
@@ -3,83 +3,16 @@
 // Wait for all DOM to be ready
 document.addEventListener('DOMContentLoaded', () => {
   const locations = document.querySelectorAll('.location');
-  const map = document.getElementsByClassName('map')[0];
   const instructionTxt = document.getElementById('instruction-text');
   let panelState = 'inactive';
   let locationName = '';
-
-  map.onload = function (evt) {
-    let selectedElement = null;
-    let offset = { x: 0, y: 0 };
-    const svg = evt.target;
-    let transform = null;
-    let bbox, minX, minY, maxX, maxY;
-    svg.addEventListener('mousedown', startDrag);
-    svg.addEventListener('mousemove', drag);
-    svg.addEventListener('mouseup', endDrag);
-    svg.addEventListener('mouseleave', endDrag);
-
-    function getMousePosition (evt) {
-      const CTM = svg.getScreenCTM();
-      return { x: (evt.clientX - CTM.e) / CTM.a, y: (evt.clientY - CTM.f) / CTM.d };
-    }
-    function startDrag (evt) {
-      selectedElement = evt.target.parentNode;
-      if (selectedElement.classList.contains('undraggable')) {
-        selectedElement = null;
-        return;
-      }
-
-      // Set drag constraint
-      if (selectedElement.classList.contains('confined')) {
-        bbox = selectedElement.getBBox();
-        minX = -100 - bbox.x;
-        maxX = 2200 - bbox.x - bbox.width;
-        minY = -30 - bbox.y;
-        maxY = 1200 - bbox.y - bbox.height;
-      }
-      offset = getMousePosition(evt);
-      const transforms = selectedElement.transform.baseVal;
-
-      // OR transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE
-      if (transforms.length === 0) {
-        // Create an transform that translates by (0, 0)
-        const translate = svg.createSVGTransform();
-        translate.setTranslate(0, 0);
-        // Add the translation to the front of the transforms list
-        selectedElement.transform.baseVal.insertItemBefore(translate, 0);
-      }
-      // Get initial translation amount
-      transform = transforms.getItem(0);
-      offset.x -= transform.matrix.e;
-      offset.y -= transform.matrix.f;
-    }
-    function drag (evt) {
-      if (selectedElement) {
-        evt.preventDefault();
-        const coord = getMousePosition(evt);
-        let dx = coord.x - offset.x;
-        let dy = coord.y - offset.y;
-
-        // Add drag constraint
-        if (selectedElement.classList.contains('confined')) {
-          if (dx < minX) { dx = minX; } else if (dx > maxX) { dx = maxX; }
-          if (dy < minY) { dy = minY; } else if (dy > maxY) { dy = maxY; }
-        }
-        transform.setTranslate(dx, dy);
-      }
-    }
-    function endDrag (evt) {
-      selectedElement = null;
-    }
-  };
 
   // Background music
   const bgm = new Audio('../assets/map-my-future-bgm.ogg'); //  eslint-disable-line
   bgm.play();
   bgm.loop = true;
 
-  // Region clicked/selected
+  // Clickable map
   locations.forEach(location => {
     location.addEventListener('click', () => {
       locationName = location.getAttribute('data-location');
