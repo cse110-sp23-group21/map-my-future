586f37566ed7116e86e1457f4cd7c661752e0650
Gil Keidar <keidar.gil@gmail.com>
2023-06-10 20:12:39 -0700

    Include basic test

--- tests/e2e/main_page/main_page.spec.js
+++ tests/e2e/main_page/main_page.spec.js
@@ -3,9 +3,40 @@ const {test, expect} = require('@playwright/test');
 
 const MAIN_PAGE_URL = 'http://127.0.0.1:5500/src/index.html';
 
-test('has title', async({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto(MAIN_PAGE_URL);
+})
 
-    //  Expect the title "Molybdomancy"
+test('has title', async({ page }) => {
+    //  Expect the title "Map My Future"
     await expect(page).toHaveTitle("Map My Future");
 });
+
+test.describe('UI Buttons', () => { 
+    test('toggle info panel on and off', async ({ page }) => {
+        //  Get info UI button
+        const infoButton = await page.locator('#info-button');
+        //  Get info panel
+        const infoPanel = await page.locator("#info-popup");
+
+        console.log(infoButton);
+        console.log(infoPanel);
+
+        //  Info panel shouldn't be open currently
+        await expect(infoPanel).not.toBeVisible();
+
+        //await page.click('music-button');
+
+        //  Click info button
+        await infoButton.click();
+
+        //  Info panel should be open currently
+        await expect(infoPanel).toBeVisible();
+
+        //  Click info button
+        await infoButton.click();
+
+        //  Info panel shouldn't be open currently
+        await expect(infoPanel).not.toBeVisible();
+    });
+})
