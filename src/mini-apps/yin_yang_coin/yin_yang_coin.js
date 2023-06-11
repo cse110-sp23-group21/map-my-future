300e6e8ca1c7687347849b786dbe984c9765affb
Jun Linwu <julinwu@ucsd.edu>
2023-06-10 23:11:54 -0700

    Minor update yin-yang-coin filepath&style.

--- src/mini-apps/yin_yang_coin/yin_yang_coin.js
+++ src/mini-apps/yin_yang_coin/yin_yang_coin.js
@@ -179,24 +179,27 @@ document.addEventListener('DOMContentLoaded', async () => {
    * Fortune reveal sound effect object
    * @type {Audio}
    */
-  const fortuneRevealSound = new Audio('../../assets/coin/hex-reveal.mp3');
+  const fortuneRevealSound = new Audio('../../assets/coin/bgm-hex-reveal.mp3');
 
   /**
    * Line reveal sound effect object
    * @type {Audio}
    */
-  const lineRevealSound = new Audio('../../assets/coin/line-reveal2.mp3');
+  const lineRevealSound = new Audio('../../assets/coin/bgm-line-reveal.mp3');
 
   /**
    * Action button press sound effect object
    * @type {Audio}
    */
-  const actionButtonPressSound = new Audio('../../assets/coin/action-button-press4.wav');
+  const actionButtonPressSound = new Audio('../../assets/coin/bgm-button-click.wav');
 
   //  Music and sound effect settings
   bgm.loop = true;
   bgm.volume = 0.4;
   flipSound.volume = 0.5;
+  fortuneRevealSound.volume = 0.5;
+  lineRevealSound.volume = 0.5;
+  actionButtonPressSound.volume = 0.5;
 
   //  Attempt to autoplay background music
   bgm.play().then(() => {
@@ -214,16 +217,6 @@ document.addEventListener('DOMContentLoaded', async () => {
    * @listens musicButton#click
    */
   musicButton.addEventListener('click', (event) => {
-    console.log('music');
-    /* const musicImg = document.getElementById('music');
-    if (musicEnabled) {
-      musicImg.src = '../../../assets/audio_off.png';
-      bgm.pause();
-    } else {
-      musicImg.src = '../../../assets/audio_on.png';
-      bgm.play();
-    }
-    musicEnabled = !musicEnabled; */
     musicEnabled = setMusicState(bgm, !musicEnabled);
   });
 
@@ -248,15 +241,13 @@ document.addEventListener('DOMContentLoaded', async () => {
    * @listens buttonElement#click
    */
   buttonElement.addEventListener('click', (event) => {
-    actionButtonPressSound.play();
-
     const buttonElement = event.target;
     const buttonValue = buttonElement.value;
-    console.log(buttonValue);
 
     // Start State
     switch (buttonValue) {
       case 'start':
+        actionButtonPressSound.play();
         // Update Button State
         buttonElement.value = 'toss';
         buttonElement.innerText = 'Toss Coins';
@@ -277,20 +268,12 @@ document.addEventListener('DOMContentLoaded', async () => {
          * Randomly generated 3-coin toss result
          */
         const coinResult = engine.get_random_subset(1)[0];  //  eslint-disable-line
-        console.log('coinResult:', coinResult.value);
-        console.log('Power of two:', powerOfTwo);
 
         //  Calculate the Hexagram Index
         hexagramIndex += coinResult.value * powerOfTwo;
         powerOfTwo = powerOfTwo << 1;
 
-        console.log('Hexagram Index:', hexagramIndex);
-
         tossCounter++;
-        console.log('Tossing coins! tossCounter =', tossCounter);
-
-
-        // UI Generation
 
         // Lines Animation
         console.log(gridList[tossCounter]);
@@ -343,15 +326,12 @@ document.addEventListener('DOMContentLoaded', async () => {
         break;
 
       case 'result':
-        // Map hexagram to intepretation
-
         fortuneRevealSound.play();
 
         /**
          * Resulting hexagram object
          */
         const hexagram = hexagrams[hexagramIndex];  //  eslint-disable-line
-        console.log('Hexagram Result:', hexagram);
 
         // Update Button State
         buttonElement.value = 'reset';
@@ -394,6 +374,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         break;
 
       case 'reset':
+        actionButtonPressSound.play();
+
         //  Reset variables relevant to hexagram generation
         hexagramIndex = 0;
         tossCounter = 0;
@@ -410,7 +392,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         character.innerHTML = '';
         intepretationTxt.innerHTML = '';
-        instructionTxt.innerHTML = 'Possible Combinations';
+        instructionTxt.innerHTML = 'Coin Combination';
         lineTxt.innerHTML = 'Side Info';
 
         character.className = 'inactive';
