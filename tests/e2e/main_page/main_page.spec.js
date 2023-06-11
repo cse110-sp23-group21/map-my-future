c740d104f1f73bc79f0057aa51fb249c23c3f21a
Gil Keidar <keidar.gil@gmail.com>
2023-06-10 19:12:24 -0700

    Added sample test

--- tests/e2e/main_page/main_page.spec.js
+++ tests/e2e/main_page/main_page.spec.js
@@ -1,5 +1,11 @@
 //  @ts-check
 const {test, expect} = require('@playwright/test');
 
-
 const MAIN_PAGE_URL = 'http://127.0.0.1:5500/src/index.html';
+
+test('has title', async({ page }) => {
+    await page.goto(MAIN_PAGE_URL);
+
+    //  Expect the title "Molybdomancy"
+    await expect(page).toHaveTitle("Map My Future");
+});
