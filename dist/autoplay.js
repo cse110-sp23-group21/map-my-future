a4d99e6e0ae2add10c2653d8185e2c312b4ba020
Gil Keidar <keidar.gil@gmail.com>
2023-06-11 21:30:14 -0700

    Removed dist/ directory to get rid of old files (CI/CD will regenerate correct files)

--- dist/autoplay.js
+++
@@ -1,1 +0,0 @@
-function setMusicState(s,t,e,a,o){return e?(t.src=void 0===a?"../../assets/audio_on.png":a,s.play()):(t.src=void 0===o?"../../assets/audio_off.png":o,s.pause()),e}export default setMusicState;
