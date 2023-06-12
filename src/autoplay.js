7bdb0946e7d1c8f29cdf06734454704117765a84
Gil Keidar <keidar.gil@gmail.com>
2023-06-11 20:05:02 -0700

    Updated autoplay.js to use relative paths

--- src/autoplay.js
+++ src/autoplay.js
@@ -4,16 +4,29 @@
  * @param {HTMLElement} musicImg HTML Element of the music UI button image
  * @param {boolean} newState Whether the background music
  *  should be turned on or off
+ * @param {string} onPath Path to the "audio_on.png" image; defaults to valid path
+ * from a mini-app folder.
+ * @param {string} offPath Path to the "audio_off.png" image; defaults to valid path
+ * from a mini-app folder.
  * @returns {boolean} the new state of the background music (set musicEnabled
  * to this)
  */
-function setMusicState (bgm, musicImg, newState) {
+function setMusicState (bgm, musicImg, newState, onPath, offPath) {
   if (!newState) {
     //  If undefined, assume path from a mini-app folder
-    musicImg.src = '/src/assets/audio_off.png';
+    if (offPath === undefined) {
+      musicImg.src = '../../assets/audio_off.png';
+    } else {
+      musicImg.src = offPath;
+    }
     bgm.pause();
   } else {
-    musicImg.src = '/src/assets/audio_on.png';
+    //  If undefined, assume path from a mini-app folder
+    if (onPath === undefined) {
+      musicImg.src = '../../assets/audio_on.png';
+    } else {
+      musicImg.src = onPath;
+    }
     bgm.play();
   }
   return newState;
